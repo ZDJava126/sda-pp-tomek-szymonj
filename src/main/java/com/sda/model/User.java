5f49ef65b41e33017a76f5caf9dd2eb16913504c
Tomek <wartego@wp.pl>
2023-01-15 12:44:28 +0100

    Task 13 - HibernateUtils entity add and User.java change_2

--- src/main/java/com/sda/model/User.java
+++ src/main/java/com/sda/model/User.java
@@ -1,12 +1,14 @@
 package com.sda.model;
 
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.Hibernate;
+import java.util.Objects;
 
 @Entity
+@RequiredArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
@@ -23,4 +25,17 @@ public class User {
     private Integer age;
     private String email;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+
+        User user = (User) o;
+        return username != null && Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
