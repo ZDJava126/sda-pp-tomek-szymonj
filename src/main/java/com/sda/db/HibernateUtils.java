6a85b42f4e3a41d063d3a7af082d3a6cc466e487
szjozi <szjozefczuk.sda@gmail.com>
2023-01-15 12:01:41 +0100

    HibernateUtils added

---
+++ src/main/java/com/sda/db/HibernateUtils.java
@@ -0,0 +1,36 @@
+package com.sda.db;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+@Slf4j
+public class HibernateUtils {
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            createSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+    private static void createSessionFactory() {
+        Configuration configuration = new Configuration();
+//                configuration.addAnnotatedClass()
+
+        final ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties())
+                .build();
+        try {
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            log.error("Creating SessionFactory failed! Exception: {}\n{}", e.getMessage(), e);
+            StandardServiceRegistryBuilder.destroy(serviceRegistry);
+        }
+    }
+
+}
